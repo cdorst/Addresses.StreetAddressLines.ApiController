5fa484871ac8e8de83006ae7b076324926714a95
Christopher Dorst <chris@dorst.email>
2018-03-29 12:18:22 -0500

    Update StreetAddressLinesController.cs

--- Addresses.StreetAddressLines.ApiController/StreetAddressLinesController.cs
+++ Addresses.StreetAddressLines.ApiController/StreetAddressLinesController.cs
@@ -4,6 +4,7 @@
 using Addresses.StreetAddressLines.DatabaseContext;
 using DevOps.Code.DataAccess.Interfaces.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Addresses.StreetAddressLines.ApiController
